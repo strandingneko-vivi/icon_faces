e5804a8bc53f2802a2989f541a4201dc37c9876a
strandingneko <strandingneko@gmail.com>
2026-09-21 08:43:05 +0800

    feat: add rtc functions.
    Signed-off-by: strandingneko <strandingneko@gmail.com>
    Signed-off-by: Zhihan Tao <zhihan.tao@telink-semi.com>

--- src/main.c
+++ src/main.c
@@ -5,13 +5,18 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/timer/nrf_grtc_timer.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
+
 int main(void)
-{
+{	
 	while (1) {
+		uint64_t now_ticks = z_nrf_grtc_timer_read();
+		uint64_t now_sec   = now_ticks / sys_clock_hw_cycles_per_sec();
+
 		k_sleep(K_MSEC(1000));
 	}
 
